e55a5938e4c94739e705f67fabecb9da7f585153
sheikhshake <sheikhmushahid_92@hotmail.com>
2022-09-30 02:04:32 +0800

    added difficiulty

--- v2/app.js
+++ v2/app.js
@@ -1,42 +1,18 @@
-console.log(pokedex[0]);
-
-/*
-
-Open game
-Main page - Have start button
-Game page - Game page should display pokemon pic.
-
-Flow 
-    - Game data, contains 151 pokemon characters
-    - On start button, display Pokemon pic, countdown timer and pokeball life
-    - Display 2 wrong and 1 correct answer
-    - If user select correct answer
-        - increase time countdown. 
-    - else
-        - decrease time and pokeball life
-
-*/
-
-// variables
-
-//pages
 const startPage = document.getElementById("startPage");
 const gamePage = document.getElementById("gamePage");
 const resultsPage = document.getElementById("resultsPage");
 
 const startButton = document.getElementById("startButton");
-const pokemonImage = document.getElementById("pokemonImage"); // <img ... />
-const pokemonImage2 = document.getElementById("pokemonImage2");
+
+const pokemonImage = document.getElementById("pokemonImage");
 const name1 = document.getElementById("name1");
 const name2 = document.getElementById("name2");
 const name3 = document.getElementById("name3");
 const restartButton = document.getElementById("restartButton");
 const finalScore = document.getElementById("finalScore");
-// const answerImage
 
 const points = document.getElementById("score");
 const timer = document.getElementById("timer");
-// const boxTimer = document.getElementById("boxTimer");
 
 const pokeBall1 = document.getElementById("ball1");
 const pokeBall2 = document.getElementById("ball2");
@@ -44,28 +20,33 @@ const pokeBall3 = document.getElementById("ball3");
 const pokeBall4 = document.getElementById("ball4");
 const pokeBall5 = document.getElementById("ball5");
 
-// sounds
-
 const sampleSound = new Audio("./pokemonBattle.mp3");
 const correctSound = new Audio("./correctAnswer.mp3");
 const wrongSound = new Audio("./wrongAnswer.mp3");
 const gameOverSound = new Audio("./gameOverSound.mp3");
 
+const g1 = Array.from(new Array(151), (x, i) => i + 1);
+const g2 = Array.from(new Array(100), (x, i) => i + 152);
+const g3 = Array.from(new Array(135), (x, i) => i + 252);
+const g4 = Array.from(new Array(107), (x, i) => i + 387);
+const g5 = Array.from(new Array(156), (x, i) => i + 494);
+const g6 = Array.from(new Array(72), (x, i) => i + 650);
+const g7 = Array.from(new Array(88), (x, i) => i + 722);
+const g8 = Array.from(new Array(96), (x, i) => i + 810);
+
 let pointsTracking,
   actualTimer,
   correctAnswer = "",
   life,
-  timerInterval;
+  timerInterval,
+  currentPokemons = [];
 
 startButton.addEventListener("click", () => {
   gameStart();
-  startButton.hidden = true;
-  //dim background
 });
 
 restartButton.addEventListener("click", () => {
-  // restartButton.disabled = true;
-  gameStart("restart");
+  gameStart();
 });
 
 name1.addEventListener("click", () => {
@@ -97,134 +78,103 @@ const game = () => {
 
   pokemonImage.classList.add("pokemonImage");
 
-  let rand = Math.floor(Math.random() * 800);
-  let rand2 = Math.floor(Math.random() * 800);
-  let rand3 = Math.floor(Math.random() * 800);
-  pokemonImage.src = pokedex[rand].image.thumbnail;
+  let i = currentPokemons[Math.floor(Math.random() * currentPokemons.length)];
+  let i2 = currentPokemons[Math.floor(Math.random() * currentPokemons.length)];
+  let i3 = currentPokemons[Math.floor(Math.random() * currentPokemons.length)];
 
-  correctAnswer = pokedex[rand].name.english;
+  pokemonImage.src = "./pokemon_images/" + pokedex[i].image;
 
-  let pokemonNames = [
-    pokedex[rand].name.english,
-    pokedex[rand2].name.english,
-    pokedex[rand3].name.english,
-  ];
+  correctAnswer = pokedex[i].name;
+
+  let pokemonNames = [pokedex[i].name, pokedex[i2].name, pokedex[i3].name];
 
   pokemonImage.classList.remove("hidden");
   name1.classList.remove("hidden");
   name2.classList.remove("hidden");
   name3.classList.remove("hidden");
 
-  console.log(pokemonNames);
-
   name1.innerHTML = pokemonNames.splice(Math.floor(Math.random() * 3), 1);
 
-  console.log(pokemonNames);
-
   name2.innerHTML = pokemonNames.splice(Math.floor(Math.random() * 2), 1);
-  console.log(pokemonNames);
 
   name3.innerHTML = pokemonNames.splice(Math.floor(Math.random() * 1), 1);
-  console.log(pokemonNames);
-
-  //if duplicate answer
-
-  // assign pokemon index to array
-
-  // check if new id is part of the existing array
-
-  // if not, continue
-
-  // if exisit, do another random
 };
 
-const gameStart = (status = null) => {
-  sampleSound.currentTime = 0;
-  sampleSound.play();
+const gameStart = () => {
+  currentPokemons = [];
+  var all_location_id = document.querySelectorAll(
+    'input[name="switch"]:checked'
+  );
 
-  // on click - start game
-  // start timer
+  var aIds = [];
 
-  pointsTracking = 0;
-  actualTimer = 30;
-  timer.innerHTML = actualTimer;
+  for (var x = 0, l = all_location_id.length; x < l; x++) {
+    aIds.push(all_location_id[x].value);
+  }
 
-  correctAnswer = "";
-  life = 5;
+  if (aIds.length !== 0) {
+    aIds.forEach((g) => {
+      if (g === "1") currentPokemons.push(...g1);
+      if (g === "2") currentPokemons.push(...g2);
+      if (g === "3") currentPokemons.push(...g3);
+      if (g === "4") currentPokemons.push(...g4);
+      if (g === "5") currentPokemons.push(...g5);
+      if (g === "6") currentPokemons.push(...g6);
+      if (g === "7") currentPokemons.push(...g7);
+      if (g === "8") currentPokemons.push(...g8);
+    });
 
-  clearInterval(timerInterval);
-  gameOverSound.pause();
-
-  console.log(status);
-  game();
-  timerInterval = setInterval(function () {
-    if (actualTimer <= 0) {
-      clearInterval(timerInterval);
-      gameEnd();
-    }
-    actualTimer -= 1;
-
+    startButton.hidden = true;
+    sampleSound.currentTime = 0;
+    sampleSound.play();
+    pointsTracking = 0;
+    actualTimer = 30;
     timer.innerHTML = actualTimer;
-    console.log(actualTimer);
-  }, 1000);
 
-  //hide start page and results page (in case its a restart game)
-  startPage.classList.add("hidden");
-  resultsPage.classList.add("hidden");
+    correctAnswer = "";
+    life = 5;
 
-  // show game page
-  gamePage.classList.remove("hidden");
-  timer.classList.remove("hidden");
-  pokeBall1.classList.remove("hidden");
-  pokeBall2.classList.remove("hidden");
-  pokeBall3.classList.remove("hidden");
-  pokeBall4.classList.remove("hidden");
-  pokeBall5.classList.remove("hidden");
+    clearInterval(timerInterval);
+    gameOverSound.pause();
 
-  // change from home page to game page
-  // show picture
-  // start counting game points
-  // remove hidden class from timer
+    game();
+    timerInterval = setInterval(function () {
+      if (actualTimer <= 0) {
+        clearInterval(timerInterval);
+        gameEnd();
+      }
+      actualTimer -= 1;
 
-  // when time is 0, end game
-  // when pokeball runs out, end game
+      timer.innerHTML = actualTimer;
+    }, 1000);
+
+    startPage.classList.add("hidden");
+    resultsPage.classList.add("hidden");
+
+    gamePage.classList.remove("hidden");
+    timer.classList.remove("hidden");
+    pokeBall1.classList.remove("hidden");
+    pokeBall2.classList.remove("hidden");
+    pokeBall3.classList.remove("hidden");
+    pokeBall4.classList.remove("hidden");
+    pokeBall5.classList.remove("hidden");
+  } else {
+    alert("SELECT GEN PLS");
+  }
 };
 
 const checkAnswer = (userInput) => {
-  buttonStatus("lock"); //skin add this function. purpose of his function is to disable the button onclick
-  // get user input
-  // check if correct
-  console.log(userInput);
-  console.log(correctAnswer);
-
   if (userInput.innerHTML === correctAnswer) {
-    console.log("correct");
     pointsTracking += 5;
     actualTimer += 3;
     pokemonImage.classList.remove("pokemonImage");
 
     userInput.style.background = "green";
     correctSound.play();
-
-    // show "CORRECT" popup
   } else {
-    console.log("wrong");
-    // pointsTracking -= 5;
     if (pointsTracking - 5 < 0) {
-      // if true
-      // if points is currently less than 5, this will run
-      // if points is 2
-      // minus 5
-      // -3
-      // set points to 0 because if not will be less than 0
       pointsTracking = 0;
     } else {
-      // if false
-      // if points is more than 5, this will run
-      // if points is 30
-      // minus 5
-      // 25
-      // minus 5 normally
       pointsTracking -= 5;
     }
 
@@ -248,8 +198,6 @@ const checkAnswer = (userInput) => {
     pokemonImage.classList.remove("pokemonImage");
     userInput.style.background = "red";
     wrongSound.play();
-
-    // show "WRONG" popup
   }
 
   points.innerHTML = pointsTracking;
@@ -258,12 +206,6 @@ const checkAnswer = (userInput) => {
       nextRound(userInput);
     }
   }, 1500);
-
-  // if correct plus points, increase timer, show next picture
-
-  // wrong minus time, minus pokeball, show next picture
-
-  //
 };
 
 const nextRound = (userInput) => {
@@ -273,7 +215,6 @@ const nextRound = (userInput) => {
 };
 
 const gameEnd = () => {
-  console.log("game over");
   finalScore.innerHTML = pointsTracking;
   pokemonImage.classList.add("hidden");
   name1.classList.add("hidden");
@@ -281,19 +222,22 @@ const gameEnd = () => {
   name3.classList.add("hidden");
   timer.classList.add("hidden");
   actualTimer = 0;
-  // stop timer, points, etc
-  // show end game screen
-  // show results
-  // hide game page
+
   gamePage.classList.add("hidden");
 
-  //show results page
   resultsPage.classList.remove("hidden");
   sampleSound.pause();
   gameOverSound.play();
 };
 
-// add audio
-// add difficulty
-
-// bindings
+const selectAll = (checked) => {
+  if (checked) {
+    document.querySelectorAll('input[name="switch"]').forEach((e) => {
+      e.checked = true;
+    });
+  } else {
+    document.querySelectorAll('input[name="switch"]').forEach((e) => {
+      e.checked = false;
+    });
+  }
+};
